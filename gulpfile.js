31925fa77bbaae1c33eac9f985660b5acbadb659
Martin Ryan <martinrryan@gmail.com>
2016-01-21 13:45:01 -0600

    gulp tasks complete - lint, process js - html, watch dist folders and reload browser

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var eslint = require('gulp-eslint');
+var stripDebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var minifyhtml = require('gulp-minify-html');
+var livereload = require('gulp-livereload');
+
+//
+//
+var src = 'public/src';
+var dist = 'public/dist';
+var javascript = 'script.js';
+var paths = {
+  js: src + '/js/*.js',
+  scss: src + '/scss/*.scss',
+  html: src + '/**/*.html'
+};
+
+//
+//
+gulp.task('lint', function () {
+  return gulp.src(paths.js)
+      // eslint() attaches the lint output to the "eslint" property
+      // of the file object so it can be used by other modules.
+      .pipe(eslint({
+        "globals":{
+          "jQuery": false,
+          "$": true
+        }
+      }))
+      // eslint.format() outputs the lint results to the console.
+      // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+      // To have the process exit with an error code (1) on
+      // lint error, return the stream and pipe to failAfterError last.
+      .pipe(eslint.failAfterError());
+
+});
+
+// Combine & Compress JS into one script.js
+gulp.task('combine-js', function () {
+  return gulp.src(paths.js)
+    // .pipe(stripDebug()) // strip all debugs and alerts
+    .pipe(concat(javascript))
+    .pipe(uglify())
+    .pipe(gulp.dest(dist + '/js'));
+});
+
+//
+// Compile sass to css
+// gulp.task('compile-sass', function () {
+//   return gulp.src(paths.scss)
+//     .pipe(sass())
+//     .pipe(gulp.dest(dist + '/css'));
+// });
+
+//
+// Compress HTML
+gulp.task('compress-html', function () {
+  return gulp.src(paths.html)
+    .pipe(minifyhtml())
+    .pipe(gulp.dest(dist + '/'));
+});
+
+// Watch files and reload browser
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(paths.js, ['combine-js']);
+  // gulp.watch(paths.scss, ['compile-sass']);
+  gulp.watch(paths.html, ['compress-html']);
+  gulp.watch(dist + '/**').on('change', livereload.changed);
+});
+
+//
+//
+gulp.task('default', [ 'lint', 'combine-js', 'compress-html', 'watch' ]);
+  //
+  //
+  // gulp.task('default', [
+  //   'lint', 'combine-js',
+  //   'compile-sass', 'compress-html',
+  //   'watch' ]);
