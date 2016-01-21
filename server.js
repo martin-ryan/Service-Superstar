c5399dad70561232e7954a5c91ea7afd85fc67e9
Martin Ryan <martinrryan@gmail.com>
2016-01-21 10:44:41 -0600

    hapi server running on port 3000 with hello world index.html

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+//
+const Hapi = require('hapi');
+const inert = require('inert');
+
+//
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 3000
+});
+
+//
+//
+server.register(inert, (err) => {
+    if (err) {
+        throw err;
+    }
+
+  server.route({
+      method: 'GET',
+      path:'/',
+      handler: function (request, reply) {
+        // return reply("hello world");
+        reply.file("index.html");
+      }
+  });
+});
+
+//
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
